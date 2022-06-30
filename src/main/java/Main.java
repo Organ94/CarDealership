a0aefe7001caa6003d2a4314f4ea84e2358d9d67
Алексей Печин <organner@mail.ru>
2022-07-01 00:07:52 +0300

    Changed adding threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Shop shop = new Shop();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             new Thread(null, shop::sellCar, "Покупатель-" + i).start();
+            new Thread(null, shop::gettingACar, "Произведитель").start();
         }
-        new Thread(null, shop::gettingACar, "Произведитель").start();
-        new Thread(null, shop::gettingACar, "Произведитель").start();
-        new Thread(null, shop::gettingACar, "Произведитель").start();
-        new Thread(null, shop::gettingACar, "Произведитель").start();
-        new Thread(null, shop::gettingACar, "Произведитель").start();
     }
 }
