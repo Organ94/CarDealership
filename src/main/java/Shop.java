5cb49f087fa24bd45a35433f02cb4f3352620309
Алексей Печин <organner@mail.ru>
2022-07-12 17:27:54 +0300

    The method of "gettingACar" changed.

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -4,20 +4,16 @@ import java.util.List;
 public class Shop {
 
     public static final int PRODUCTION_TIME = 2500;
-    public static final int TIME_OF_PURCHASE = 2500;
+    public static final int TIME_OF_PURCHASE = 2000;
 
     List<Car> cars = new ArrayList<>();
 
-    public synchronized void gettingACar() {
-        try {
-            while (cars.size() < 10) {
-                Thread.sleep(PRODUCTION_TIME);
-                cars.add(new Car());
-                System.out.println(Thread.currentThread().getName() + " выпустил автомобиль BMW");
-                notify();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public synchronized void gettingACar() throws InterruptedException {
+        Thread.sleep(PRODUCTION_TIME);
+        while (cars.size() < 10) {
+            cars.add(new Car());
+            System.out.println(Thread.currentThread().getName() + " выпустил автомобиль BMW");
+            notify();
         }
     }
 
