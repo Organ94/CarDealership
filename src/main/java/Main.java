b0d8edf2ea234e9a3bbd45d0d5b888a00d93e110
Алексей Печин <organner@mail.ru>
2022-07-12 17:24:33 +0300

    Changed the creation of the "Произведитель".

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Shop shop = new Shop();
 
         for (int i = 0; i < 10; i++) {
             new Thread(null, shop::sellCar, "Покупатель-" + i).start();
         }
-        new Thread(null, shop::gettingACar, "Произведитель").start();
+        new Thread(null, () -> {
+            try {
+                shop.gettingACar();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "Произведитель").start();
     }
 }
